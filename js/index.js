95e8fa9bbc0753652ff73affcfc9c154b2f83d33
Sara Silva <saracarolinasilva@gmail.com>
2022-10-05 13:05:15 +0200

    iteration_1 done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // Iteration 1: Names and Input
 //
+console.log("I'm ready!");
+const hacker1 = "Leonardo";
+console.log(`The driver's name is ${hacker1}.`);
+const hacker2 = "Sara";
+console.log(`The navigator's name is ${hacker2}.`);
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
